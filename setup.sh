989a265a97b35e3165753ebc16ad90b62a1d8549
Cleberson Faccin <cfaccin@adaptavist.com>
2018-07-03 22:12:34 -0300

    JQA-6642 jira configuration setup

--- setup.sh
+++ setup.sh
@@ -14,4 +14,6 @@ sleep 5
 echo "restarting..."
 sleep 5
 java -jar jenkins-cli.jar -s http://localhost:8080/jenkins create-job tm4j-cucumber-jenkins-plugins-test < config.xml
+cp com.adaptavist.tm4j.jenkins.Tm4jReporter.xml work/com.adaptavist.tm4j.jenkins.Tm4jReporter.xml
+java -jar jenkins-cli.jar -s http://localhost:8080/jenkins restart
 rm jenkins-cli.jar
