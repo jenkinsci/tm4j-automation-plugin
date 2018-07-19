ef7cfdf43f58f81d52ac5c1a76fbf2018c760bdb
Cleberson Faccin <cfaccin@adaptavist.com>
2018-07-19 11:03:09 -0300

    JQA-6677 adjust setup for async update of jenkins plugins

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 if [ 200 == $(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8080/jenkins/jnlpJars/jenkins-cli.jar) ]
 then
@@ -9,40 +10,57 @@ then
 fi
 
 rm -r work/
+mvn clean
 
 sh ./run.sh &
 
+echo "starting..."
 while [ 200 != $(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8080/jenkins/jnlpJars/jenkins-cli.jar) ]
 do
-	echo "starting..."
 	sleep 1
 done
 
-
 curl http://localhost:8080/jenkins/jnlpJars/jenkins-cli.jar --output jenkins-cli.jar
-sleep 1
+echo "wait..."
+java -jar jenkins-cli.jar -s http://localhost:8080/jenkins safe-restart
+
+echo "stoping..."
+while [ 200 == $(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8080/jenkins/jnlpJars/jenkins-cli.jar) ]
+do
+	sleep 1
+done
+
+echo "starting..."
+while [ 200 != $(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8080/jenkins/jnlpJars/jenkins-cli.jar) ]
+do
+	sleep 1
+done
+
 java -jar jenkins-cli.jar -s http://localhost:8080/jenkins install-plugin git
+sleep 1
+echo "restarting..."
 java -jar jenkins-cli.jar -s http://localhost:8080/jenkins restart
 
 while [ 200 != $(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8080/jenkins/jnlpJars/jenkins-cli.jar) ]
 do
-	echo "restarting..."
 	sleep 1
 done
 
 java -jar jenkins-cli.jar -s http://localhost:8080/jenkins create-job tm4j-cucumber-jenkins-plugins-test < config.xml
 cp com.adaptavist.tm4j.jenkins.Tm4jReporter.xml work/com.adaptavist.tm4j.jenkins.Tm4jReporter.xml
+echo "restarting..."
 java -jar jenkins-cli.jar -s http://localhost:8080/jenkins restart
 
 while [ 200 != $(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8080/jenkins/jnlpJars/jenkins-cli.jar) ]
 do
-	echo "restarting..."
 	sleep 1
 done
 
 echo "Done"
 java -jar jenkins-cli.jar -s http://localhost:8080/jenkins safe-shutdown
 
-echo "Jenkins Stoped"
+sleep 2
+echo "Jenkins stoped"
+echo "Setup finished"
 echo "Execute run.sh to run Jenkins";
 #rm jenkins-cli.jar
